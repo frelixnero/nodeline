19a86b1442d5cd4a6572150f28f923efa7c6cdca
frelixnero <osiraogene@gmail.com>
2026-07-16 13:23:43 +0100

    13 Workflows UI

--- src/features/workflows/hooks/use-workflow.ts
+++ src/features/workflows/hooks/use-workflow.ts
@@ -5,11 +5,14 @@ import { TRPCClientError } from "@trpc/client";
 import { useMutation, useQueryClient, useSuspenseQuery } from "@tanstack/react-query";
 import { useRouter } from "next/navigation";
 import { toast } from "sonner";
+import { useWorkflowsParams } from "./use-workflows-params";
+import { tr } from "date-fns/locale";
 
 export const useSuspenseWorkflows = () => {
     const trpc = useTRPC();
+    const [ params] = useWorkflowsParams();
 
-    return useSuspenseQuery(trpc.workflows.getMany.queryOptions());
+    return useSuspenseQuery(trpc.workflows.getMany.queryOptions(params));
 }
 
 
@@ -20,7 +23,7 @@ export  const useCreateWorkflow = () => {
     return useMutation(trpc.workflows.create.mutationOptions({
         onSuccess: (data) => {
              toast.success(`Workflow "${data.name}" created successfully`);  
-             queryClient.invalidateQueries(trpc.workflows.getMany.queryOptions());
+             queryClient.invalidateQueries(trpc.workflows.getMany.queryOptions({}));
         },
         onError: (error) => {
             if (error instanceof TRPCClientError && error.data?.code === "FORBIDDEN") {
@@ -29,4 +32,20 @@ export  const useCreateWorkflow = () => {
             toast.error(`Error creating workflow: ${error.message}`);
         }
     }))
+}
+
+
+//  hook to remove a workflow
+export const useRemoveWorkflow = () => {
+    const queryClient = useQueryClient();
+    const trpc = useTRPC();
+
+    return useMutation(
+        trpc.workflows.remove.mutationOptions({
+            onSuccess: (data) => {
+                toast.success(`Workflow "${data.name}" removed successfully`);
+                queryClient.invalidateQueries(trpc.workflows.getMany.queryOptions({})); 
+            }
+        })
+    )
 }
